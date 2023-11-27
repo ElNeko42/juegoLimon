01bb5f7efd8aa86402db3fa47656c4f6da1529b6
ElNeko42 <franson177@gmail.com>
2023-11-27 12:33:35 +0100

    textos menu estadisticas personaje

--- Assets/Scripts/Player/TextControl.cs
+++ Assets/Scripts/Player/TextControl.cs
@@ -6,11 +6,20 @@ using UnityEngine;
 public class TextControl : MonoBehaviour
 {
     public TextMeshProUGUI nombreTextMesh;
+    public TextMeshProUGUI nombreMenuTextMesh;
+    public TextMeshProUGUI feTextMesh;
+    public TextMeshProUGUI fuerzaTextMesh;
+    public TextMeshProUGUI conocimientoTextMesh;
+    public TextMeshProUGUI suerteTextMesh;
 
     void Start()
     {
         nombreTextMesh.text = GameController.instance.playerName;
-
+        nombreMenuTextMesh.text = GameController.instance.playerName;
+        feTextMesh.text ="Fe: "+ GameController.instance.playerFaith.ToString();
+        fuerzaTextMesh.text ="Fuerza: "+ GameController.instance.playerStrength.ToString();
+        conocimientoTextMesh.text ="Conocimiento: "+ GameController.instance.playerKnowledge.ToString();
+        suerteTextMesh.text ="Suerte: "+ GameController.instance.playerLuck.ToString();
     }
 
 }
